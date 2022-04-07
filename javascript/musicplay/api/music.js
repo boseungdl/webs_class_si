6ae02149e9a2ddc7a5d3b61732af341f4177f1c5
boseungdl <96tmdqh@naver.com>
2022-04-07 17:20:32 +0900

    123

---
+++ javascript/musicplay/api/music.js
@@ -0,0 +1,56 @@
+let allMusic = [
+    {
+        name : "i'm your man",
+        artist : "Leonard Cohen",
+        img : "../images/music01",
+        audio : "music-1" 
+    },
+    {
+        name : "Extreme Ways",
+        artist : "Mody",
+        img : "../images/music02",
+        audio : "music-2" 
+    },
+    {
+        name : "beyond",
+        artist : "Jone Mickal",
+        img : "../images/music03",
+        audio : "music-3" 
+    },
+    {
+        name : "sky",
+        artist : "Jone Mickal",
+        img : "../images/music04",
+        audio : "music-4" 
+    },
+    {
+        name : "bottom",
+        artist : "Jone Mickal",
+        img : "../images/music05",
+        audio : "music-5" 
+    },
+    {
+        name : "top",
+        artist : "Jone Mickal",
+        img : "../images/music06",
+        audio : "music-6" 
+    },
+    {
+        name : "shell we dance?",
+        artist : "Jone Mickal",
+        img : "../images/music07",
+        audio : "music-7" 
+    },
+    {
+        name : "dynamite",
+        artist : "love",
+        img : "../images/music08",
+        audio : "music-8" 
+    },
+    {
+        name : "happy",
+        artist : "Jone Mickal",
+        img : "../images/music09",
+        audio : "music-9" 
+    },
+]
